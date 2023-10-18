daa309f81db9559b49599d99ae60313e4c9da46f
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-10-18 03:36:03 +0200

    Update index.tsx

--- src/components/protectedRoute/index.tsx
+++ src/components/protectedRoute/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { useAuth } from 'hooks/useAuth';
 import { CLosedSection } from './styles';
 
-export default function ProtectedRoute({ children }): JSX.Element {
+export default function ProtectedRoute({ children }: any): JSX.Element {
   const isAuth = useAuth();
 
-  return <>{isAuth ? children : <CLosedSection />}</>;
+  return <>{isAuth ? { children } : <CLosedSection />}</>;
 }
